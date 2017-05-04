77470785a1984552d8d82a47d05237b1cf24c6ff
jeremyfromearth <jeremynealbrown@gmail.com>
2017-05-03 22:52:28 -0700

    Added operators and methods to cell class

--- include/hex.h
+++ include/hex.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <math.h>
+#include <stdlib.h>
 #include <stdexcept>
 #include <stdint.h>
+#include <string>
 
 namespace hex {
     class cell  { 
@@ -14,11 +17,19 @@ namespace hex {
 
             cell operator-(const cell& rhs) const;
 
+            cell operator*(float scalar) const;
+
+            bool operator==(const cell& rhs) const;
+
+            uint32_t distance(const cell& other);
+
             int32_t get_x() const { return x; }
 
             int32_t get_y() const { return y; }
 
             int32_t get_z() const { return z; }
+
+            std::string to_string();
 
         private:
             int32_t x, y, z;
--- src/hex.cpp
+++ src/hex.cpp
@@ -17,3 +17,21 @@ cell cell::operator+(const cell& rhs) const {
 cell cell::operator-(const cell& rhs) const {
     return cell(x - rhs.get_x(), y - rhs.get_y(), z - rhs.get_z());
 }
+
+cell cell::operator*(float scalar) const {
+    return cell(round(x * scalar), round(y * scalar), round(z * scalar));
+}
+
+bool cell::operator==(const cell& rhs) const {
+    return x == rhs.get_x() && y == rhs.get_y() && z == rhs.get_z();
+}
+
+uint32_t cell::distance(const cell& other) {
+    return floor((abs(x - other.get_x()) + abs(y - other.get_y()) + abs(z - other.get_z())) / 2);
+}
+
+std::string cell::to_string() {
+    return "Cell(" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ")";
+}
+
+
